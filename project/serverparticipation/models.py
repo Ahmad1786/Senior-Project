2c0a7ed87fa728c433fbf826c6ecb9252e5dbf4d
Daniel Reger <danielreger6@gmail.com>
2024-03-06 23:34:14 -0500

    Changed strings, sizes, removed cascade, fixed User

--- project/serverparticipation/models.py
+++ project/serverparticipation/models.py
@@ -1,27 +1,27 @@
 from django.contrib.auth.models import AbstractUser 
 from django.db import models
+from django.conf import settings
 import datetime
-# Create your models here.
 
 class Server():
-    group_name = models.CharField(max_length=100)
+    group_name = models.CharField(max_length=30)
     group_icon = models.BinaryField(null=True, blank=True)
     date_created = models.DateField(auto_now_add=True)
 
 
     def __str__(self):
-        return self.group_name
+        return "Group: " + self.group_name + ", created on " + self.date_created
 
 class Participation():
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    server = models.ForeignKey(Server, on_delete=models.CASCADE)
-    display_name = models.CharField(max_length=100)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL)
+    server = models.ForeignKey(Server)
+    display_name = models.CharField(max_length=30)
     date_joined = models.DateField(auto_now_add=True)
     points = models.IntegerField(default=0)
     is_owner = models.BooleanField(default=False)
 
     def __str__(self):
-        return self.display_name | self.server.group_name
+        return "User: " + self.self.user.name + ", joined " + self.server.group_name + " on " + self.date_joined
 
 
 
