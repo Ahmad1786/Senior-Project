bf84375475d3301db7fb60242e3a1966e5501c37
LukeLach76 <157992508+LukeLach76@users.noreply.github.com>
2024-03-06 23:58:34 -0500

    Update models.py

--- project/users/models.py
+++ project/users/models.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.models import AbstractUser 
 from phonenumber_field.modelfields import PhoneNumberField
+from django.db import models
 
 # extend the User model by extending AbstractUser: see link for details
 # https://docs.djangoproject.com/en/5.0/topics/auth/customizing/#using-a-custom-user-model-when-starting-a-project
