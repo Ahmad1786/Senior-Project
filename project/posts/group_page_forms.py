c770e01de829b0279ba104590740ed4970ac76f8
Muhammad Ahmad <69284724+Ahmad1786@users.noreply.github.com>
2024-03-17 21:32:23 -0400

    feat: finish initial aspect of group page
    - can now view all posts for server/group
    - and create a new post using modal (yes modal not model) forms

--- project/posts/group_page_forms.py
+++ project/posts/group_page_forms.py
@@ -1,11 +1,12 @@
 # THESE ARE THE INITIAL FORMS FOR THE SERVERS APP
-# THESE WILL BE USED TO MAKE THE 3 MAIN POSTS IN THE GROUP PAGE
+# THESE WILL BE USED TO MAKE an instance of THE 3 MAIN POSTS IN THE GROUP PAGE
 
 from django import forms
 from posts.models import Bill, Event, Chore
 from servers.models import Server
+import datetime
 
-# NOTE: MADE these forms kind of fast, so they may not be the 
+# NOTE: MADE these forms kind of "fast", so they may not be the 
 # best/most efficient way to do things, but good enough for now
 
 class BillForm(forms.ModelForm):
@@ -56,11 +57,7 @@ class BillForm(forms.ModelForm):
         fields = ['post_name', 'description', 'cost', 'split','payee', 'server', 'creator']
 
 
-
-
-
 class TaskForm(forms.ModelForm):
-    pass
     def __init__(self, *args, **kwargs):
         self.request = kwargs.pop('request', None)
         self.current_server_id = kwargs.pop('current_server_id', None)
@@ -72,31 +69,48 @@ class TaskForm(forms.ModelForm):
         self.fields['server'].widget = forms.HiddenInput()
         self.fields['creator'].widget = forms.HiddenInput()
         self.fields['creator'].initial = self.request.user
-        
-        self.fields['payee'].queryset = server_instance.members.all()
-  
-        self.fields['payee'].initial = self.request.user
+
+        self.fields['assignee'].queryset = server_instance.members.all()
+
+        self.fields['due_date'].initial = datetime.datetime.now().strftime('%Y-%m-%d')
         
     def clean(self):
         cleaned_data = super().clean()
-        
+        # validation if needed can go here
+        return cleaned_data
+    class Meta:
+        model = Chore
+        fields = ['post_name', 'description',  'server', 'creator', 'assignee', 'due_date']
+        widgets = {
+            'due_date':forms.TextInput(attrs={'type':'date'}),
+        }
 
-        
-        if not cleaned_data['split'] and len(cleaned_data['payee']) > 1:
-            self.add_error('payee', 'Can not split bill between multiple people if split is not checked')
 
-        if cleaned_data['split'] and len(cleaned_data['payee']) == 1:
-            self.add_error('payee', 'Must split bill between multiple people if split is checked')
-
-        # Decided this may not be what we want but basically i was trying to say
-        # if split is false: the user making the post must be the only one paying
-        # if not cleaned_data['split']:
-        #    print(cleaned_data['payee'])
-        #    print(self.request.user)
-        #    cleaned_data['payee'] = [self.request.user]
+class EventForm(forms.ModelForm):
+    def __init__(self, *args, **kwargs):
+        self.request = kwargs.pop('request', None)
+        self.current_server_id = kwargs.pop('current_server_id', None)
+        server_instance = Server.objects.get(pk=self.current_server_id)
+        super(EventForm, self).__init__(*args, **kwargs)
         
+        # initial server value
+        self.fields['server'].initial = self.current_server_id
+        self.fields['server'].widget = forms.HiddenInput()
+        self.fields['creator'].widget = forms.HiddenInput()
+        self.fields['creator'].initial = self.request.user
+
+        # label for datetime
+        self.fields['date_time'].label = "Date and Time"
+        self.fields['date_time'].initial = datetime.datetime.now().strftime("%Y-%m-%dT%H:%M")
+        
+    def clean(self):
+        cleaned_data = super().clean()
+        # validation if needed can go here
         return cleaned_data
 
     class Meta:
-        model = Bill
-        fields = ['post_name', 'description', 'split','payee', 'server', 'creator']
+        model = Event
+        fields = ['post_name', 'description', 'date_time', 'server', 'creator']
+        widgets = {
+            'date_time':forms.TextInput(attrs={'type':'datetime-local'}),
+        }
--- project/servers/htmx_views.py
+++ project/servers/htmx_views.py
@@ -1,17 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from posts.group_page_forms import BillForm
+from posts.group_page_forms import BillForm, EventForm, TaskForm
 
 
-# These views will be used to create the modal forms
-# that pop up in the main group page to create a new post
+# These views will be used to create the modal forms that pop up
+# in the main group page to create a new post
 
 def add_bill(request, server_id):
     if request.method == 'POST':
         form = BillForm(request.POST, request=request, current_server_id=server_id)
         if form.is_valid():
             form.save()
-            return HttpResponse(status=204, headers={'HX-Trigger': 'billListChanged'})
+            return HttpResponse(status=204, headers={'HX-Trigger': 'NewPostAdded'})
         else:
             return render(request, 'servers/partials/bill-form.html', {
                 'form': form,
@@ -22,8 +22,36 @@ def add_bill(request, server_id):
         'form': form,
     })
 
+
 def add_task(request, server_id):
-    return HttpResponse(f"Adding a task for server {server_id}!")
+    if request.method == 'POST':
+        form = TaskForm(request.POST, request=request, current_server_id=server_id)
+        if form.is_valid():
+            form.save()
+            return HttpResponse(status=204, headers={'HX-Trigger': 'NewPostAdded'})
+        else:
+            return render(request, 'servers/partials/task-form.html', {
+                'form': form,
+            })
+    
+    form = TaskForm(request=request, current_server_id=server_id)
+    return render(request, 'servers/partials/task-form.html', {
+        'form': form,
+    })
+
 
 def add_event(request, server_id):
-    return HttpResponse(f"Adding an event for server {server_id}!")
+    if request.method == 'POST':
+        form = EventForm(request.POST, request=request, current_server_id=server_id)
+        if form.is_valid():
+            form.save()
+            return HttpResponse(status=204, headers={'HX-Trigger': 'NewPostAdded'})
+        else:
+            return render(request, 'servers/partials/event-form.html', {
+                'form': form,
+            })
+    
+    form = EventForm(request=request, current_server_id=server_id)
+    return render(request, 'servers/partials/event-form.html', {
+        'form': form,
+    })
