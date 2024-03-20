ac08cd99b86c652cb707490f457e328aa1039c53
Daniel Reger <danielreger6@gmail.com>
2024-03-20 15:44:57 -0400

    Created views for bills, chores, and events. Still working on them but largely done. Made some bare bones html pages.

--- project/posts/urls.py
+++ project/posts/urls.py
@@ -10,7 +10,10 @@ testing_routes = [
     path('test-chore/<int:id>', test_posts.test_chore, name="test_chore"),
     path('test-event/<int:id>', test_posts.test_event, name="test_event"),
     path('test-comment/<int:id>', test_posts.test_comment, name="test_comment"),
-    path('bill/<int:id>', views.bill, name="bill")
+    path('bill/<int:id>', views.bill, name="bill"),
+    path('chore/<int:id>', views.chore, name="chore"),
+    path('event/<int:id>', views.event, name="event"),
+    path('comment/<int:id>', views.comment, name="comment")
 ]
 
 htmx_routes = []
--- project/posts/views.py
+++ project/posts/views.py
@@ -1,11 +1,12 @@
 from django.shortcuts import render
 from .models import Bill, Chore, Event, Comment
+from users.models import User
 
 def bill(request, id):
 
     bill = Bill.objects.get(id=id)
     
-    creator = bill.creator
+    post_name = bill.post_name
     description = bill.description
     date_created = bill.date_created
     cost = bill.cost
@@ -16,11 +17,55 @@ def bill(request, id):
     #payee = bill.payee
 
     return render(request, "posts/bill.html", {
-        "bill": bill,
-        "creator": creator,
+        "post_name": post_name,
         "description": description,
         "date_created": date_created,
         "cost": cost,
         "split": split,
-        "individual_portion": individual_portion
+        "individual_portion": individual_portion,
+        "payers": payers
     })
+
+def chore(request, id):
+
+    chore = Chore.objects.get(id=id)
+    
+    post_name = chore.post_name
+    description = chore.description
+    date_created = chore.date_created
+    due_date = chore.due_date
+    assignees = chore.assignee.all()
+    # Fix later
+    assigned_to = [assignee.first_name for assignee in assignees]
+    creator = chore.creator
+
+    return render(request, "posts/chore.html", {
+        "post_name": post_name,
+        "description": description,
+        "date_created": date_created,
+        "due_date": due_date,
+        "assigned_to": assigned_to,
+        "creator": creator,
+    })
+
+def event(request, id):
+    
+    event = Event.objects.get(id=id)
+
+    post_name = event.post_name
+    description = event.description
+    date_created = event.date_created
+    time = event.date_time
+    creator = event.creator
+
+    return render(request, "posts/event.html", {
+        "post_name": post_name,
+       "description": description,
+       "date_created": date_created,
+       "time": time,
+       "creator": creator, 
+    })
+
+def comment(request, id):
+
+    comment = Comment.objects.get(id=id)
