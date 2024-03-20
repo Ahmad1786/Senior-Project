8a54227f8e08e141a73800ca1757c1fe478762b8
LukeLach76 <157992508+LukeLach76@users.noreply.github.com>
2024-03-19 20:26:55 -0400

    added urls for editing such as edit-task, edit-bill, and edit-event to servers/urls.py

--- project/servers/urls.py
+++ project/servers/urls.py
@@ -8,8 +8,12 @@ urlpatterns = [
 
 htmx_routes = [
     path('add-bill/<int:server_id>', htmx_views.add_bill, name="add_bill"),
+    path('edit-bill/<int:bill_id>', htmx_views.edit_bill, name= "edit_bill"),
     path('add-task/<int:server_id>', htmx_views.add_task, name="add_task"),
+    path ('edit-task/<int:task_id>', htmx_views.edit_task, name="edit_task"),
     path('add-event/<int:server_id>', htmx_views.add_event, name="add_event"),
+    path('edit-event/<int:event_id>', htmx_views.edit_event, name ="edit_event")
+    
 ]
 
 urlpatterns += htmx_routes
