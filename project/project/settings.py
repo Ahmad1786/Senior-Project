5163d59f52ad1b846e27b9c4c1aaa7645aebe806
Daniel Reger <danielreger6@gmail.com>
2024-03-27 13:26:32 -0400

    Removed uncessary folder and modified some settings

--- project/project/settings.py
+++ project/project/settings.py
@@ -14,7 +14,8 @@ from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-STATICFILES_DIRS = [BASE_DIR / "static"]
+STATICFILES_DIRS = [BASE_DIR / "posts/static"]
+STATIC_URL = "posts/static"
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
