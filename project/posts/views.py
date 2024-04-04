d2b999d06fb547f25000a6eed4db7a9e720b47b4
Daniel Reger <danielreger6@gmail.com>
2024-04-04 16:37:44 -0400

    Made buttons rather than hyperlinks. Started implementing edit features, made url and started view.

--- project/posts/views.py
+++ project/posts/views.py
@@ -170,6 +170,11 @@ def add_comment(request, post_type, post_id):
         "post_id": post_id,
     })
 
+
+def edit_comment(request, post_type, post_id, comment_id):
+    comment = Comment.objects.get(id=comment_id)
+    
+
 def delete_comment(request, post_type, post_id, comment_id):
     if request.method == "POST":
         comment = Comment.objects.get(id=comment_id)
