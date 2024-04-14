f80aee0db37b5767fabd24b3427e18849a63d475
Muhammad Ahmad <69284724+Ahmad1786@users.noreply.github.com>
2024-04-14 12:11:49 -0400

    fix: avoid full page refresh on notifications page

--- project/users/notification_views.py
+++ project/users/notification_views.py
@@ -2,7 +2,9 @@ from django.shortcuts import render
 from users.models import Notification, User
 from django.http import HttpResponse
 from django.shortcuts import redirect
+from django.contrib.auth.decorators import login_required
 
+@login_required
 def notification_page(request):
     notifications = request.user.notifications.order_by('-created').all()
     return render(request, 'users/notifications.html', {'notifications': notifications})
@@ -16,4 +18,4 @@ def mark_as_read(request, notification_id):
     n = Notification.objects.get(pk=notification_id)
     n.read = True
     n.save()
-    return redirect('users:notification_page')
+    return render(request, 'users/notification_item.html', {'n': n})
